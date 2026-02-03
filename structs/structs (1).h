171f8c32fe9fcc3e5268bdfb2095c7e44b2bef4b
annabely0 <annabely2005@gmail.com>
2026-02-02 23:34:19 -0300

    Update structs (1).h

--- structs/structs (1).h
+++ structs/structs (1).h
@@ -1,12 +1,1 @@
-#ifndef STRUCTS_H
-#define STRUCTS_H
 
-typedef struct Produto {
-    int codigo;
-    char nome[50];
-    float preco;
-    int quantidade;
-    struct Produto *prox;
-} Produto;
-
-#endif
